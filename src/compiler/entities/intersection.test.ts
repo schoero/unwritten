c4a81d2ffc29b60da4cb125232b4c06bff268348
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2022-11-20 10:22:16 +0100

    test: finished test for `Intersection`

--- src/compiler/entities/intersection.test.ts
+++ src/compiler/entities/intersection.test.ts
@@ -4,33 +4,78 @@ import { compile } from "../../../tests/utils/compile.js";
 import { scope } from "../../../tests/utils/scope.js";
 import { ts } from "../../../tests/utils/template.js";
 import { Intersection, TypeKind } from "../../types/types.js";
+import { getIdBySymbol } from "../compositions/id.js";
 import { createTypeAliasBySymbol } from "./alias.js";
 
 
 scope("Compiler", TypeKind.Intersection, () => {
 
-  const testFileContent = ts`
-    type A = {
-      a: string;
-    };
-    type B = {
-      b: string;
-    };
-    export type IntersectionType = A & B;
-  `;
+  {
 
-  const { exportedSymbols, ctx } = compile(testFileContent.trim());
+    const testFileContent = ts`
+      export type IntersectionType = { a: string } & { b: number };
+    `;
 
-  const symbol = exportedSymbols.find(s => s.name === "IntersectionType")!;
-  const exportedVariable = createTypeAliasBySymbol(ctx, symbol);
+    const { exportedSymbols, ctx } = compile(testFileContent.trim());
 
-  it("should have a matching kind", () => {
-    expect(exportedVariable.kind).to.equal(TypeKind.TypeAlias);
-    expect(exportedVariable.type.kind).to.equal(TypeKind.Intersection);
-  });
+    const symbol = exportedSymbols.find(s => s.name === "IntersectionType")!;
+    const exportedTypeAlias = createTypeAliasBySymbol(ctx, symbol);
 
-  it("should have the right amount of types", () => {
-    expect((exportedVariable.type as Intersection).types).to.have.lengthOf(2);
-  });
+    it("should be able to parse an intersection type", () => {
+      expect(exportedTypeAlias.kind).to.equal(TypeKind.TypeAlias);
+      expect(exportedTypeAlias.type.kind).to.equal(TypeKind.Intersection);
+    });
+
+  }
+
+  {
+
+    const testFileContent = ts`
+      /**
+       * Intersection type description
+       * @example { a: string } & { b: number }
+       */
+      export type IntersectionType = { a: string } & { b: number };
+    `;
+
+    const { exportedSymbols, ctx } = compile(testFileContent.trim());
+
+    const symbol = exportedSymbols.find(s => s.name === "IntersectionType")!;
+    const exportedTypeAlias = createTypeAliasBySymbol(ctx, symbol);
+
+    it("should have a matching kind", () => {
+      expect(exportedTypeAlias.kind).to.equal(TypeKind.TypeAlias);
+      expect(exportedTypeAlias.type.kind).to.equal(TypeKind.Intersection);
+    });
+
+    it("should have a matching name", () => {
+      expect(exportedTypeAlias.name).to.equal("IntersectionType");
+    });
+
+    it("should have a matching id", () => {
+      expect(exportedTypeAlias.id).to.equal(getIdBySymbol(ctx, symbol));
+    });
+
+    it("should have a matching description", () => {
+      expect(exportedTypeAlias.description).to.equal("Intersection type description");
+    });
+
+    it("should have a matching example", () => {
+      expect(exportedTypeAlias.example).to.equal("{ a: string } & { b: number }");
+    });
+
+    it("should have a matching position", () => {
+      expect(exportedTypeAlias.position).to.deep.equal({
+        column: 6,
+        file: "/file.ts",
+        line: 5
+      });
+    });
+
+    it("should have the right amount of types", () => {
+      expect((exportedTypeAlias.type as Intersection).types).to.have.lengthOf(2);
+    });
+
+  }
 
 });
