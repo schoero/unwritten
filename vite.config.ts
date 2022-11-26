40c9072951b9bd6d18e05be133103c862680029e
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2022-11-26 08:32:29 +0100

    ci: flip test reporters

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import GithubActionsReporter from "vitest-github-actions-reporter";
 export default defineConfig({
   test: {
     reporters: process.env.GITHUB_ACTIONS
-      ? ["verbose", new GithubActionsReporter()]
+      ? [new GithubActionsReporter(), "verbose"]
       : "default"
   }
 });
