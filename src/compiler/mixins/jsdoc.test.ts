6343e0137df9728c59ba1c327c191bde3c536be6
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-01-18 20:43:44 +0100

    test: add JSDoc tests

---
+++ src/compiler/mixins/jsdoc.test.ts
@@ -0,0 +1,41 @@
+import { expect, it } from "vitest";
+
+import { createTypeAliasEntity } from "quickdoks:compiler:entities";
+import { compile } from "quickdoks:tests:utils/compile.js";
+import { scope } from "quickdoks:tests:utils/scope.js";
+import { ts } from "quickdoks:tests:utils/template.js";
+
+
+scope("Compiler", "JSDoc", () => {
+
+  {
+
+    const testFileContent = ts`
+      /**
+       * Type description
+       * @remarks This is a longer description.
+       * @alpha This type is in alpha.
+       * @beta This type is in beta.
+       * @deprecated This type is deprecated.
+       * @example type example
+       */
+      export type Test = true;
+    `;
+
+    const { exportedSymbols, ctx } = compile(testFileContent);
+
+    const symbol = exportedSymbols.find(s => s.name === "Test")!;
+    const exportedTypeAlias = createTypeAliasEntity(ctx, symbol);
+
+    it("should have all matching JSDoc tags", () => {
+      expect(exportedTypeAlias.description).to.equal("Type description");
+      expect(exportedTypeAlias.remarks).to.equal("This is a longer description.");
+      expect(exportedTypeAlias.alpha).to.equal("This type is in alpha.");
+      expect(exportedTypeAlias.beta).to.equal("This type is in beta.");
+      expect(exportedTypeAlias.deprecated).to.equal("This type is deprecated.");
+      expect(exportedTypeAlias.example).to.equal("type example");
+    });
+
+  }
+
+});
