fb74f886c7f679451f256e95ba7cacb525346568
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-04-10 21:17:55 +0200

    refactor: fixup remove typescript renderer temporarily

--- src/renderer/markup/utils/config.ts
+++ src/renderer/markup/utils/config.ts
@@ -3,19 +3,15 @@ import type { HTMLRenderContext, MarkdownRenderContext, MarkupRenderContexts } f
 
 import type { JSONRenderConfig } from "unwritten:renderer:json/type-definitions/config.js";
 import type { JSONRenderContext } from "unwritten:renderer:json/type-definitions/renderer.js";
-import type { TypeScriptRenderConfig } from "unwritten:renderer:ts/type-definitions/config.js";
-import type { TypeScriptRenderContext } from "unwritten:renderer:ts/type-definitions/renderer.js";
 import type { Complete } from "unwritten:type-definitions/utils.js";
 
 
 export function getRenderConfig(ctx: MarkdownRenderContext): Complete<MarkdownRenderConfig>;
 export function getRenderConfig(ctx: HTMLRenderContext): Complete<HTMLRenderConfig>;
 export function getRenderConfig(ctx: MarkupRenderContexts): Complete<MarkupRenderConfig>;
-export function getRenderConfig(ctx: TypeScriptRenderContext): Complete<TypeScriptRenderConfig>;
 export function getRenderConfig(ctx: JSONRenderContext): Complete<JSONRenderConfig>;
-export function getRenderConfig(ctx: MarkupRenderContexts | TypeScriptRenderContext): Complete<MarkupRenderConfig | TypeScriptRenderConfig>;
-export function getRenderConfig(ctx: JSONRenderContext | MarkupRenderContexts | TypeScriptRenderContext): Complete<JSONRenderConfig | MarkupRenderConfig | TypeScriptRenderConfig>;
-export function getRenderConfig(ctx: JSONRenderContext | MarkupRenderContexts | TypeScriptRenderContext) {
+export function getRenderConfig(ctx: JSONRenderContext | MarkupRenderContexts): Complete<JSONRenderConfig | MarkupRenderConfig>;
+export function getRenderConfig(ctx: JSONRenderContext | MarkupRenderContexts) {
   const name = ctx.renderer.name;
   return ctx.config.renderConfig[name];
 }
