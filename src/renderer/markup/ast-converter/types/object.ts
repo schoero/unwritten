94c25f92a4336ed5e38ac8889b00200f6398e237
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-04-11 07:57:45 +0200

    fix: do not render title in types

--- src/renderer/markup/ast-converter/types/object.ts
+++ src/renderer/markup/ast-converter/types/object.ts
@@ -3,8 +3,7 @@ import {
   convertPropertyEntityForDocumentation,
   convertSignatureEntityForDocumentation
 } from "unwritten:renderer/markup/ast-converter/entities/index.js";
-import { createTitleNode } from "unwritten:renderer/markup/utils/nodes.js";
-import { getTranslator } from "unwritten:renderer/markup/utils/translations.js";
+import { createListNode } from "unwritten:renderer/markup/utils/nodes.js";
 
 import type {
   InterfaceType,
@@ -26,8 +25,6 @@ export function convertObjectType(ctx: MarkupRenderContexts, typeLiteralType: Ty
 export function convertObjectType(ctx: MarkupRenderContexts, objectType: ObjectType): ConvertedObjectType;
 export function convertObjectType(ctx: MarkupRenderContexts, objectLikeType: InterfaceType | ObjectLiteralType | ObjectType | TypeLiteralType): ConvertedObjectType {
 
-  const translate = getTranslator(ctx);
-
   const convertedProperties = objectLikeType.properties.map(propertyEntity => convertPropertyEntityForDocumentation(ctx, propertyEntity));
   const convertedConstructSignatures = objectLikeType.constructSignatures.map(signatureEntity => convertSignatureEntityForDocumentation(ctx, signatureEntity));
   const convertedCallSignatures = objectLikeType.callSignatures.map(signatureEntity => convertSignatureEntityForDocumentation(ctx, signatureEntity));
@@ -35,13 +32,13 @@ export function convertObjectType(ctx: MarkupRenderContexts, objectLikeType: Int
   const convertedGetters = objectLikeType.getters.map(getterEntity => convertFunctionLikeEntityForDocumentation(ctx, getterEntity));
   const convertedMethods = objectLikeType.methods.map(methodEntity => convertFunctionLikeEntityForDocumentation(ctx, methodEntity));
 
-  return [
-    createTitleNode(translate("construct-signature", { count: 99 }), undefined, convertedConstructSignatures),
-    createTitleNode(translate("call-signature", { count: 99 }), undefined, convertedCallSignatures),
-    createTitleNode(translate("property", { count: 99 }), undefined, convertedProperties),
-    createTitleNode(translate("method", { count: 99 }), undefined, convertedMethods),
-    createTitleNode(translate("setter", { count: 99 }), undefined, convertedSetters),
-    createTitleNode(translate("getter", { count: 99 }), undefined, convertedGetters)
-  ];
+  return createListNode([
+    convertedConstructSignatures,
+    convertedCallSignatures,
+    convertedProperties,
+    convertedMethods,
+    convertedSetters,
+    convertedGetters
+  ]);
 
 }
