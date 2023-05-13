88694821dbce3b52c93bc604f806e6bac01d095a
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-05-13 12:26:13 +0200

    test: add utilty function to debug

---
+++ tests/utils/debug.ts
@@ -0,0 +1,16 @@
+export function getEnumFlagNames(enumObj: any, flags: number) {
+  const allFlags = Object.keys(enumObj)
+    .map(key => enumObj[key])
+    .filter(value => typeof value === "number") as number[];
+  const matchedFlags = allFlags.filter(flag => (flag & flags) !== 0);
+
+  return matchedFlags
+    .filter((flag, index) => matchedFlags.indexOf(flag) === index)
+    .map(f => {
+      const power = Math.log2(f);
+      if(Number.isInteger(power)){
+        return `${enumObj[f]} (2 ^ ${power})`;
+      }
+      return enumObj[f];
+    });
+}
