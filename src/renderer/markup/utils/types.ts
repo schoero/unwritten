4f3becc07f07d9e7cca81c6d2a370feb8f732dc1
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-06-18 12:57:21 +0200

    fix: add missing multiline types

--- src/renderer/markup/utils/types.ts
+++ src/renderer/markup/utils/types.ts
@@ -1,10 +1,20 @@
-import { isFunctionType, isObjectType, isTypeLiteralType } from "unwritten:typeguards/types.js";
+import {
+  isClassType,
+  isFunctionType,
+  isInterfaceType,
+  isObjectLiteralType,
+  isObjectType,
+  isTypeLiteralType
+} from "unwritten:typeguards/types.js";
 
 import type { MultilineTypes, Types } from "unwritten:interpreter/type-definitions/types.js";
 
 
 export function isMultilineType(type: Types): type is MultilineTypes {
   return isObjectType(type) ||
+  isObjectLiteralType(type) ||
   isFunctionType(type) ||
-  isTypeLiteralType(type);
+  isTypeLiteralType(type) ||
+  isClassType(type) ||
+  isInterfaceType(type);
 }
