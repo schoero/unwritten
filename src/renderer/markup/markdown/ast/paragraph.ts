c533374766549116ecd2529f0681475e673fdb09
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-08-24 11:48:27 +0200

    fix(paragraph): add indentation

--- src/renderer/markup/markdown/ast/paragraph.ts
+++ src/renderer/markup/markdown/ast/paragraph.ts
@@ -1,3 +1,4 @@
+import { renderIndentation } from "unwritten:renderer/utils/indentation.js";
 import { renderNode } from "unwritten:renderer:markdown/index.js";
 
 import type { MarkdownRenderContext } from "unwritten:renderer:markup/types-definitions/markup.js";
@@ -5,8 +6,11 @@ import type { ParagraphNode } from "unwritten:renderer:markup/types-definitions/
 
 
 export function renderParagraphNode(ctx: MarkdownRenderContext, paragraphNode: ParagraphNode): string {
+
+  const renderedIndentation = renderIndentation(ctx);
+
   const renderedNode = renderNode(ctx, paragraphNode.children);
   return renderedNode === ""
     ? renderedNode
-    : `${renderedNode}  `;
+    : `${renderedIndentation}${renderedNode}  `;
 }
