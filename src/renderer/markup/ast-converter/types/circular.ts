780ece45e86daf1c39338ad099d20709e5fbe59e
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-10-03 06:45:36 +0200

    fix: circular type not encapsulated

--- src/renderer/markup/ast-converter/types/circular.ts
+++ src/renderer/markup/ast-converter/types/circular.ts
@@ -1,4 +1,7 @@
+import { renderNode } from "unwritten:renderer/index.js";
 import { createAnchorNode } from "unwritten:renderer/markup/utils/nodes.js";
+import { encapsulate } from "unwritten:renderer/markup/utils/renderer.js";
+import { getRenderConfig } from "unwritten:renderer/utils/config.js";
 
 import type { CircularType } from "unwritten:interpreter:type-definitions/types.js";
 import type { MarkupRenderContexts } from "unwritten:renderer:markup/types-definitions/markup.js";
@@ -7,18 +10,22 @@ import type { ConvertedCircularTypeInline } from "unwritten:renderer:markup/type
 
 export function convertCircularTypeInline(ctx: MarkupRenderContexts, circularType: CircularType): ConvertedCircularTypeInline {
 
+  const renderConfig = getRenderConfig(ctx);
+
   const name = circularType.name ?? "";
+  const renderedName = encapsulate(name, renderConfig.typeEncapsulation);
+  const renderedEncapsulatedName = renderNode(ctx, renderedName);
 
   if(circularType.symbolId){
 
-    const anchor = circularType.symbolId
-      ? createAnchorNode(name, circularType.symbolId)
-      : undefined;
+    const anchor = circularType.symbolId && createAnchorNode(name, circularType.symbolId, renderedEncapsulatedName);
 
-    return anchor ?? name;
+    if(anchor){
+      return anchor;
+    }
 
   }
 
-  return name;
+  return renderedEncapsulatedName;
 
 }
